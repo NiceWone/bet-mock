f08fb8bb25f4977b6d1cbc6ec143afc5584c172c
NiceWone <gowa36484@gmail.com>
2018-06-15 14:21:26 +0300

    - full mock matches

--- src/app/model/mock-matches.ts
+++ src/app/model/mock-matches.ts
@@ -2,6 +2,7 @@ import {Match} from './match';
 import {TEAMS} from './mock-teams';
 
 export const MATCHES: Match[] = [
+  // GROUP A
   {
     id: 0,
     firstTeam: TEAMS[0],
@@ -44,7 +45,9 @@ export const MATCHES: Match[] = [
     scoreFirstTeam: 0,
     scoreSecondTeam: 0,
     date: new Date()
-  }, {
+  },
+  // GROUP B
+  {
     id: 6,
     firstTeam: TEAMS[7],
     secondTeam: TEAMS[6],
@@ -83,6 +86,270 @@ export const MATCHES: Match[] = [
     id: 11,
     firstTeam: TEAMS[6],
     secondTeam: TEAMS[4],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  },
+  // GROUP C
+  {
+    id: 12,
+    firstTeam: TEAMS[8],
+    secondTeam: TEAMS[10],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 13,
+    firstTeam: TEAMS[11],
+    secondTeam: TEAMS[9],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 14,
+    firstTeam: TEAMS[9],
+    secondTeam: TEAMS[10],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 15,
+    firstTeam: TEAMS[8],
+    secondTeam: TEAMS[11],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 16,
+    firstTeam: TEAMS[9],
+    secondTeam: TEAMS[8],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 17,
+    firstTeam: TEAMS[10],
+    secondTeam: TEAMS[11],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  },
+  // GROUP D
+  {
+    id: 18,
+    firstTeam: TEAMS[14],
+    secondTeam: TEAMS[13],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 19,
+    firstTeam: TEAMS[12],
+    secondTeam: TEAMS[15],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 20,
+    firstTeam: TEAMS[14],
+    secondTeam: TEAMS[12],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 21,
+    firstTeam: TEAMS[15],
+    secondTeam: TEAMS[13],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 22,
+    firstTeam: TEAMS[15],
+    secondTeam: TEAMS[14],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 23,
+    firstTeam: TEAMS[13],
+    secondTeam: TEAMS[12],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  },
+  // GROUP E
+  {
+    id: 24,
+    firstTeam: TEAMS[19],
+    secondTeam: TEAMS[17],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 25,
+    firstTeam: TEAMS[18],
+    secondTeam: TEAMS[16],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 26,
+    firstTeam: TEAMS[18],
+    secondTeam: TEAMS[19],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 27,
+    firstTeam: TEAMS[17],
+    secondTeam: TEAMS[16],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 28,
+    firstTeam: TEAMS[16],
+    secondTeam: TEAMS[19],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 29,
+    firstTeam: TEAMS[17],
+    secondTeam: TEAMS[18],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  },
+  // GROUP F
+  {
+    id: 30,
+    firstTeam: TEAMS[20],
+    secondTeam: TEAMS[23],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 31,
+    firstTeam: TEAMS[21],
+    secondTeam: TEAMS[22],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 32,
+    firstTeam: TEAMS[22],
+    secondTeam: TEAMS[23],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 33,
+    firstTeam: TEAMS[20],
+    secondTeam: TEAMS[21],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 34,
+    firstTeam: TEAMS[22],
+    secondTeam: TEAMS[20],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 35,
+    firstTeam: TEAMS[23],
+    secondTeam: TEAMS[21],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  },
+  // GROUP G
+  {
+    id: 36,
+    firstTeam: TEAMS[25],
+    secondTeam: TEAMS[27],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 37,
+    firstTeam: TEAMS[26],
+    secondTeam: TEAMS[24],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 38,
+    firstTeam: TEAMS[25],
+    secondTeam: TEAMS[26],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 39,
+    firstTeam: TEAMS[24],
+    secondTeam: TEAMS[27],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 40,
+    firstTeam: TEAMS[24],
+    secondTeam: TEAMS[25],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 41,
+    firstTeam: TEAMS[27],
+    secondTeam: TEAMS[26],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  },
+  // GROUP H
+  {
+    id: 42,
+    firstTeam: TEAMS[29],
+    secondTeam: TEAMS[30],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 43,
+    firstTeam: TEAMS[28],
+    secondTeam: TEAMS[31],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 44,
+    firstTeam: TEAMS[30],
+    secondTeam: TEAMS[31],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 45,
+    firstTeam: TEAMS[28],
+    secondTeam: TEAMS[29],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 46,
+    firstTeam: TEAMS[31],
+    secondTeam: TEAMS[29],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 47,
+    firstTeam: TEAMS[30],
+    secondTeam: TEAMS[28],
     scoreFirstTeam: 0,
     scoreSecondTeam: 0,
     date: new Date()
