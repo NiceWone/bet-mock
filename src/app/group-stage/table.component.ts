1babb67c67ed2e04e9bc4f57766bb184b8137b66
NiceWone <gowa36484@gmail.com>
2018-06-15 05:52:22 +0300

    - mock matches group a,b  - click but for test

--- src/app/group-stage/table.component.ts
+++ src/app/group-stage/table.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {log} from 'util';
+import {MATCHES} from '../model/mock-matches';
+import {Match} from '../model/match';
 
 @Component({
   selector: 'app-table',
@@ -9,9 +12,20 @@ export class TableComponent implements OnInit {
 
   @Input() group;
 
+  matches: Match[];
+
   constructor() {
   }
 
   ngOnInit() {
+    this.matches = MATCHES;
+  }
+
+  showDetails(id: number) {
+    const start = id * 6;
+    const end = start + 6;
+    for (let i = start; i < end; i++) {
+      log(this.matches[i].firstTeam.name + ' : ' + this.matches[i].secondTeam.name);
+    }
   }
 }
--- src/app/model/mock-matches.ts
+++ src/app/model/mock-matches.ts
@@ -6,7 +6,7 @@ export const MATCHES: Match[] = [
     id: 0,
     firstTeam: TEAMS[0],
     secondTeam: TEAMS[3],
-    scoreFirstTeam: 5,
+    scoreFirstTeam: 0,
     scoreSecondTeam: 0,
     date: new Date()
   }, {
@@ -41,6 +41,48 @@ export const MATCHES: Match[] = [
     id: 5,
     firstTeam: TEAMS[3],
     secondTeam: TEAMS[1],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 6,
+    firstTeam: TEAMS[7],
+    secondTeam: TEAMS[6],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 7,
+    firstTeam: TEAMS[4],
+    secondTeam: TEAMS[5],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 8,
+    firstTeam: TEAMS[4],
+    secondTeam: TEAMS[7],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 9,
+    firstTeam: TEAMS[6],
+    secondTeam: TEAMS[5],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 10,
+    firstTeam: TEAMS[5],
+    secondTeam: TEAMS[7],
+    scoreFirstTeam: 0,
+    scoreSecondTeam: 0,
+    date: new Date()
+  }, {
+    id: 11,
+    firstTeam: TEAMS[6],
+    secondTeam: TEAMS[4],
     scoreFirstTeam: 0,
     scoreSecondTeam: 0,
     date: new Date()
