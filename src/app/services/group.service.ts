0e26d1c9e87f22d6c68b3f22c93584e4933272b0
NiceWone <gowa36484@gmail.com>
2018-06-18 06:34:44 +0300

    - refactor services:  - tap logging  - error handling

--- src/app/services/group.service.ts
+++ src/app/services/group.service.ts
@@ -1,9 +1,9 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Group} from '../model/group';
 import {HttpClient} from '@angular/common/http';
 import {Match} from '../model/match';
-import {map} from 'rxjs/operators';
+import {catchError, map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,22 +13,49 @@ export class GroupService {
   private groupsUrl = 'api/groups';
   private matchesUrl = 'api/matches';
 
-  getGroups(): Observable<Group[]> {
-    return this.http.get<Group[]>(this.groupsUrl);
-  }
-
   constructor(private http: HttpClient) {
   }
 
-  getGroup(id: number): Observable<Group> {
-    return this.http.get<Group>(`${this.groupsUrl}/${id}`);
+  getGroups(): Observable<Group[]> {
+    return this.http.get<Group[]>(this.groupsUrl)
+      .pipe(
+        tap(() => console.log(`fetched groups`)),
+        catchError(this.handleError('getGroups', []))
+      );
   }
 
+  /** GET group by id. Will 404 if id not found */
+  getGroup(id: number): Observable<Group> {
+    return this.http.get<Group>(`${this.groupsUrl}/${id}`)
+      .pipe(
+        tap(() => console.log(`fetched group id=${id}`)),
+        catchError(this.handleError('getGroup', [])));
+  }
+
+  /** GET matches by id. Will 404 if id not found */
   getMatches(id: number): Observable<Match[]> {
     return this.http.get<Match[]>(`${this.matchesUrl}`)
       .pipe(
         map(matches =>
-          matches.filter(x => (x.id >= id * 6 && x.id < id * 6 + 6))
-        ));
+          matches.filter(x => (x.id >= id * 6 && x.id < id * 6 + 6))),
+        tap(() => console.log(`fetched matches for group  id=${id}`)),
+        catchError(this.handleError('getMatches', []))
+      );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error); // log to console instead
+      console.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
   }
 }
--- src/app/services/match.service.ts
+++ src/app/services/match.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Match} from '../model/match';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,38 @@ export class MatchService {
 
   private matchesUrl = 'api/matches';
 
-  getMatches(): Observable<Match[]> {
-    return this.http.get<Match[]>(this.matchesUrl);
-  }
-
   constructor(private http: HttpClient) {
   }
 
+  getMatches(): Observable<Match[]> {
+    return this.http.get<Match[]>(this.matchesUrl)
+      .pipe(
+        tap(() => console.log(`fetched matches`)),
+        catchError(this.handleError('getMatches', []))
+      );
+  }
+
+  /** GET match by id. Will 404 if id not found */
   getMatch(id: number): Observable<Match> {
-    return this.http.get<Match>(`${this.matchesUrl}/${id}`);
+    return this.http.get<Match>(`${this.matchesUrl}/${id}`)
+      .pipe(
+        tap(() => console.log(`fetched match id=${id}`)),
+        catchError(this.handleError('getMatch', [])));
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error); // log to console instead
+      console.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
   }
 }
