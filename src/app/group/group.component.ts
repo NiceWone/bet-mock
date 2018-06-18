c1f163df666a67d2f404b3eec3560524fd4fa100
NiceWone <gowa36484@gmail.com>
2018-06-18 05:13:01 +0300

    - sort table group after load

--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Match} from '../model/match';
 import {GroupService} from '../services/group.service';
 import {Group} from '../model/group';
-import {MatchService} from '../services/match.service';
 
 @Component({
   selector: 'app-table',
@@ -12,52 +11,51 @@ import {MatchService} from '../services/match.service';
 export class GroupComponent implements OnInit {
 
   groups: Group[] = [];
-  matches: Match[] = [];
 
   constructor(
-    private groupsService: GroupService,
-    private matchService: MatchService) {
+    private groupService: GroupService) {
   }
 
   ngOnInit() {
     this.getGroups();
-    this.getMatches();
-    this.calculateAllGroups();
   }
 
   getGroups(): void {
-    this.groupsService.getGroups()
-      .subscribe(groups => this.groups = groups);
-  }
-
-  getMatches(): void {
-    this.matchService.getMatches()
-      .subscribe(match => this.matches = match);
+    this.groupService.getGroups()
+      .subscribe(groups => {
+        this.groups = groups;
+        this.calculateAllGroups();
+      });
   }
 
   private calculateAllGroups() {
-    this.groups.forEach(e => this.calculateGroup(e));
-    this.groups.forEach(e => this.sortTeams(e));
+    this.groups.forEach(group => this.calculateGroup(group));
   }
 
   private calculateGroup(gr: Group) {
-    const start = gr.id * 6;
-    const end = start + 6;
+    this.groupService.getMatches(gr.id)
+      .subscribe(matches => {
+        matches.forEach(match => this.calc(match, gr));
+        this.sortTeams(gr);
+      });
+  }
 
-    for (let i = start; i < end; i++) {
-      const match = this.matches[i];
-      if (match.scoreFirstTeam != null && match.scoreSecondTeam != null) {
-        match.firstTeam.matches++;
-        match.secondTeam.matches++;
+  private calc(match: Match, gr: Group): void {
 
-        if (match.scoreFirstTeam === match.scoreSecondTeam) {
-          match.firstTeam.points++;
-          match.secondTeam.points++;
-        } else if (match.scoreFirstTeam > match.scoreSecondTeam) {
-          match.firstTeam.points += 3;
-        } else {
-          match.secondTeam.points += 3;
-        }
+    const team1 = gr.teams.find(x => x.id === match.firstTeam.id);
+    const team2 = gr.teams.find(x => x.id === match.secondTeam.id);
+
+    if (match.scoreFirstTeam != null && match.scoreSecondTeam != null) {
+      team1.matches++;
+      team2.matches++;
+
+      if (match.scoreFirstTeam === match.scoreSecondTeam) {
+        team1.points++;
+        team2.points++;
+      } else if (match.scoreFirstTeam > match.scoreSecondTeam) {
+        team1.points += 3;
+      } else {
+        team2.points += 3;
       }
     }
   }
